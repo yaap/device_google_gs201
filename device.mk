e9c41031ff7662ce0ff65e655d7b82e6994bd1c6
Michael Bestas <mkbestas@lineageos.org>
2025-12-02 13:46:03 +0200

    gs201: Rework sepolicy
    TODO: Automatically handle the following:
    * certs/mac_permissions.xml/keys.conf
    
    Change-Id: Idb0636bce2392beb720e420055a7bcb838725a18

--- device.mk
+++ device.mk
@@ -705,8 +705,6 @@ include hardware/google/pixel/common/pixel-common-device.mk
 # Wifi ext
 include hardware/google/pixel/wifi_ext/device.mk
 
-include device/google/gs201/dumpstate/item.mk
-
 # Keymint configuration
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
