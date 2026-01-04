da08a0059a23c081d522d1695d00995a95818170
sidharthify <wednisegit@gmail.com>
2026-01-04 11:11:35 +0000

    gs201: Adapt conditional AVB keys to the new path
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- BoardConfigYAAP.mk
+++ BoardConfigYAAP.mk
@@ -29,9 +29,9 @@ AB_OTA_PARTITIONS += \
 # Verified Boot
 ifneq (user,$(TARGET_BUILD_VARIANT))
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-else ifneq (,$(wildcard .android-certs/releasekey.key))
+else ifneq (,$(wildcard vendor/yaap/signing/keys/releasekey.key))
 BOARD_AVB_ALGORITHM := SHA256_RSA2048
-BOARD_AVB_KEY_PATH := .android-certs/releasekey.key
+BOARD_AVB_KEY_PATH := vendor/yaap/signing/keys/releasekey.key
 else
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 endif
