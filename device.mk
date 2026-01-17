885a423b9bd61a9042f6587351d2efb65054586d
sidharthify <wednisegit@gmail.com>
2026-01-17 08:17:02 +0000

    gs201: revert 'use the Natural display color mode by default'
    People prefer vivid
    This reverts commit https://github.com/yaap/device_google_gs201/commit/fe4741df54dd7be50956b658975cfad1e7960418
    
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- device.mk
+++ device.mk
@@ -434,7 +434,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.protected_contents=true
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.surface_flinger.display_update_imminent_timeout_ms=50
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.sf.color_saturation=1.0
+	persist.sys.sf.native_mode=2
 
 # limit DPP downscale ratio
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.hwc.dpp.downscale=2
